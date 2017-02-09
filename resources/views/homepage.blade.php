53271cf78b89bb3e346302731a642b9de33b1961
Shikhar Basnet <basnet.shikhar@gmail.com>
2017-02-09 11:31:17 +0545

    pashuram view added

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -495,7 +495,62 @@
         var map = new google.maps.Map(mapCanvas, mapOptions);
     }
 </script>
+<script type="text/javascript">
+    function study(url) {
+        $.ajax({
+            url:url,
+            method:'get',
+            success:function (result) {
+                $('.bodycontent').html('');
+                $('.bodycontent').append(result);
+                $("#homemenu").attr('onclick',"loadHome('1');");
+                $("#aboutmenu").attr('onclick',"loadHome('2');");
+                $("#servicesmenu").attr('onclick',"loadHome('3');");
+                $("#contactmenu").attr('onclick',"loadHome('4');");
+                $('html,body').animate({
+                    scrollTop: 0
+                });
+            },
+            error:function (result) {
 
+            }
+        });
+    }
+
+    function loadHome(menu) {
+        $.ajax({
+            url:'/index',
+            method:'get',
+            success:function (result) {
+                $('.bodycontent').html('');
+                $('.bodycontent').append(result);
+                $('.carousel').carousel({
+                    interval: 2000 //changes the speed
+                });
+                myMap();
+                if(menu == '1'){
+                    $("#homemenu").attr('onclick',"scrollUpHome();");
+                    scrollUpHome();
+                }
+                else if(menu == '2'){
+                    $("#aboutmenu").attr('onclick',"scrollDownAboutUs();");
+                    scrollDownAboutUs();
+                }
+                else if(menu == '3'){
+                    $("#servicesmenu").attr('onclick',"scrollDownCompanuFeature();");
+                    scrollDownCompanuFeature();
+                }
+                else if(menu == '4'){
+                    $("#contactmenu").attr('onclick',"scrollDownContact();");
+                    scrollDownContact();
+                }
+            },
+            error:function (result) {
+                console.log(result);
+            }
+        });
+    }
+</script>
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAZL5YdXuEIRgO6G4eUH1rBHOfYhy0rmMk&callback=myMap"></script>
 </body>
 </html>
